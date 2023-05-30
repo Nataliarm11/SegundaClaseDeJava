5cde73a5ce36f895ee0c6e556fd11e94ed539008
Nataliarm11 <nrequenam1@gmail.com>
2023-05-30 15:32:26 -0500

    primer commit java

---
+++ EjemploClases/src/main/java/com/mindhub/EjemploClases/EjemploClasesApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.EjemploClases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploClasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploClasesApplication.class, args);
+	}
+
+}
